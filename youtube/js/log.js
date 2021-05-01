38df2ebae99796e541982a64b23431039a22fd73
ibrahimKadri <78695030+ibrahimKadri@users.noreply.github.com>
2021-05-01 18:05:14 +0300

    Delete youtube directory

--- youtube/js/log.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    // حذف الفيديو من سجل المشاهدة عندما يُضغط على إشارة الخطأ الموجودة في الزاوية
-    $(".remove").click(function () {
-        $(this).parents('.card').hide(); 
-    });
-});
